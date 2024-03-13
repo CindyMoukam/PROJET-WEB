ac39954e982e52325518a4a4b76263006095faa8
SOFT <cindy.kouedeu@2027.ucac-icam.com>
2024-03-13 09:37:39 +0100

    app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const EventRoutes = require('./routes/Event');
 const likeRoutes = require('./routes/like');
 const categoryRoutes = require('./routes/category');
 const commentRoutes = require('./routes/comment');
-const commentmessage = require('./routes/message');
+const messageRoutes = require('./routes/message');
 
 app.use(express.json());
 app.use(cors());
@@ -25,17 +25,13 @@ app.use("/idea", ideaRoutes);
 app.use("/image", imageRoutes);
 app.use("/users", EventRoutes);
 app.use("/product", likeRoutes);
-<<<<<<< HEAD
 app.use("/product", messageRoutes);
 //app.use("/idea", categoryRoutes);
 app.use("/comment", commentRoutes);
 //app.use("/image", imageRoutes);
-app.use("/category", categoryRoutes);
-
-=======
 app.use("/comment", commentRoutes);
 app.use("/category", categoryRoutes);
->>>>>>> 3e3cda52d2d2a7a6896f55a06ec94e5b2584cb8e
+
 
 
 // Export the module to use it the server
